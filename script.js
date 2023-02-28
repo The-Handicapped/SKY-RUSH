acafccee566c8a86af722d982ea80c0a49d85319
OrwRandal <randypichardo1987@gmail.com>
2023-02-28 15:00:44 -0500

    mvp2 done

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ kaboom({
     stretch: true,
     letterbox: true,
 });
-
 loadSound('boing', 'boing.mp3');
-loadSprite("stick", "stick.png");
-loadSprite("bg", "bg.jpg");
-loadSprite('logo', 'Logo.png')
-
+loadSound('bgMusic', 'backgroundMusic.mp3')
+loadSprite("stick", "sprites/stick.png");
+loadSprite("bg", "sprites/bg.jpg");
+loadSprite('logo', 'sprites/Logo.png');
+loadSprite('homeStick', 'sprites/homeStick.png');
+loadSprite('cloud', 'sprites/cloud.png')
 scene('start', () => {
     currentScene = 'start';
     let background = add([
@@ -21,14 +22,13 @@ scene('start', () => {
         }),
         z(0)
     ])
-
     let currTab = true;
-
     const clouds = () => {
         if (currentScene === "start" && currTab) {
             add([
-                rect(100, 10),
-                pos(width(), Math.floor(Math.random() * height())),
+                sprite('cloud'),
+                pos(width() + 100, Math.floor(Math.random() * height())),
+                scale(.03, .03),
                 origin('center'),
                 area(),
                 move(LEFT, 50),
@@ -36,10 +36,8 @@ scene('start', () => {
             ])
         }
     }
-
     clouds()
     let cloudGenerator = setInterval(clouds, 5000);
-
     let logo = add([
         sprite('logo'),
         pos(width() / 2, height() / 4),
@@ -47,31 +45,32 @@ scene('start', () => {
         scale(.3, .3),
         z(2)
     ])
+    let character = add([
+        sprite('homeStick'),
+        pos(width() / 2, height() / 2 + 40),
+        scale(.2, .2),
+        origin('center')
+    ])
     let startButton = add([
         text('Click here to start'),
-        pos(width() / 2, height() / 2),
+        pos(width() / 2, height() - 90),
         origin('center'),
         scale(.5, .5),
         area(),
         z(2),
         'start'
     ])
-
     window.addEventListener("blur", () => {
         currTab = false;
     });
     window.addEventListener("focus", () => {
         currTab = true;
     });
-
     startButton.onClick(() => {
         go('game')
     });
-
 })
-
 go('start')
-
 scene('gameOver', () => {
     currentScene = 'gameOver';
     let retryButton = add([
@@ -82,23 +81,19 @@ scene('gameOver', () => {
         area(),
         'start'
     ])
-
     retryButton.onClick(() => {
         go('game')
     });
-
 })
-
 scene('game', () => {
+    // play('bgMusic', {
+    //     loop: true,
+    //     volume: 0.3
+    // })
     currentScene = 'game';
+    let deathCounter = 0;
+    let firstJump = true;
     let score = 0;
-    let background = add([
-        sprite("bg", {
-            width: width(),
-            height: height()
-        })
-    ])
-
     let player = add([
         sprite("stick"),
         pos(width() / 2, 0),
@@ -106,15 +101,51 @@ scene('game', () => {
         area(),
         body(),
         solid(),
+        z(1),
         'char'
     ])
-
+    let bg = add([
+        sprite("bg", {
+            width: width() * 2,
+            height: height()
+        }),
+        pos(width() / 2, player.pos.y),
+        origin('center'),
+        z(0)
+    ])
+    player.onUpdate(() => {
+        camPos(center().x, player.pos.y)
+    })
+    bg.onUpdate(() => {
+        bg.pos.y = player.pos.y;
+    });
+    setInterval(() => {
+        deathCounter++;
+        if (deathCounter === 3 && !firstJump) {
+            destroy(player);
+            every('platformTag', (platforms) => {
+                destroy(platforms);
+            });
+            clearInterval(platformGenerator);
+            go('gameOver');
+        }
+        else if(deathCounter === 6 && firstJump){
+            destroy(player);
+            every('platformTag', (platforms) => {
+                destroy(platforms);
+            });
+            clearInterval(platformGenerator);
+            go('gameOver');
+        }
+    }, 1000)
     let scoreBoard = add([
         text(score),
         pos(20, 20),
         scale(.5, .5)
     ])
-
+    scoreBoard.onUpdate(() => {
+        scoreBoard.pos.y = player.pos.y - 230
+    })
     let floor = add([
         rect(width(), 10),
         pos(width() / 2, height()),
@@ -122,7 +153,6 @@ scene('game', () => {
         area(),
         solid()
     ])
-
     let startFloor = add([
         rect(width(), 10),
         color('black'),
@@ -133,54 +163,58 @@ scene('game', () => {
         move(DOWN, 50),
         'startFloor'
     ])
-
     let lastPosY = player.pos.y;
+    let currHeight = height();
 
     const platform = () => {
         if (currentScene === "game") {
             add([
-                rect(Math.floor(Math.random() * 200) + 100, 10),
-                pos(Math.floor(Math.random() * width()), 0),
+                // sprite('cloud'),
+                // scale(.03,.03),
+                rect(Math.floor(Math.random() * 100) + 100, 10),
+                pos(Math.floor(Math.random() * width()), currHeight - 115),
                 origin('center'),
                 area(),
                 // solid(),
-                move(DOWN, 100),
+                // move(DOWN, 100),
                 'platformTag'
-            ])
+            ]);
+            currHeight -= 115;
         }
     }
-
     onUpdate(() => {
         lastPosY = player.pos.y;
-      });
-
+    });
     player.onCollide("platformTag", () => {
         if (player.pos.y > lastPosY) {
-          player.jump(500);
+            player.jump(500);
+            firstJump = false;
+            deathCounter = 0;
+            // play('boing', {
+            //     volume: 0.1
+            // })
         }
-      });
-
+    });
     floor.onCollide("platformTag", (platform) => {
         destroy(platform);
         score += 100;
         scoreBoard.text = score;
     })
-
+    floor.onUpdate(() => {
+        floor.pos.y = player.pos.y + 500
+    })
     floor.onCollide('startFloor', (start) => {
         destroy(start);
     })
-
-    floor.onCollide('char', (character) => {
-        destroy(character);
-        every('platformTag', (platforms) => {
-            destroy(platforms);
-        });
-        clearInterval(platformGenerator);
-        go('gameOver');
-    })
-
-    let platformGenerator = setInterval(platform, 1000);
-
+    // floor.onCollide('char', (character) => {
+    //     destroy(character);
+    //     every('platformTag', (platforms) => {
+    //         destroy(platforms);
+    //     });
+    //     clearInterval(platformGenerator);
+    //     go('gameOver');
+    // })
+    let platformGenerator = setInterval(platform, 500);
     player.onUpdate(() => {
         if (player.pos.x >= width()) {
             player.pos.x = 0
@@ -189,60 +223,34 @@ scene('game', () => {
             player.pos.x = width();
         }
     })
-
-<<<<<<< HEAD
-    // player jump change
-    onUpdate(() => {
-        lastPosY = player.pos.y;
-    });
-
-    player.onUpdate(() => {
-        camPos() = player.pos
-    })
-    player.onUpdate(() => {
-        if (player.pos.y > lastPosY) {
-            player.jump(650);
-        }
-    });
-
-    // onUpdate(() => {
-    //     camPos({ x: width() / 2, y: player.pos.y });
-    // });
-
-=======
->>>>>>> 15464a95f77a9371195d5f69772f02bee9f36b49
     // player.onUpdate(() => {
     //     if (player.isGrounded()) {
     //         player.jump();
     //         play('boing')
     //     }
     // })
-
     gravity(900);
-
     // let SPEED = 900;
 
-    player.onCollide('startFloor', () => {
-        onKeyDown("space", () => {
+    onKeyDown("space", () => {
+        if (firstJump) {
             player.jump(650);
-        })
+            firstJump = false;
+        }
     })
+
     // onKeyDown("space", () => {
     //     player.jump(650);
     // })
-
     onKeyDown("left", () => {
         player.move(-400, 0);
     })
-
     onKeyDown("a", () => {
         player.move(-400, 0);
     })
-
     onKeyDown("right", () => {
         player.move(400, 0)
     })
-
     onKeyDown("d", () => {
         player.move(400, 0)
     })
